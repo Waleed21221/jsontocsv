39581e1da2d2c89cb94b87659ab77042126e56bd
Waleed <waleed.ahmed@z2climited.com>
2024-05-23 14:59:27 +0500

    first commit

--- api/hello.ts
+++ api/hello.ts
@@ -2,7 +2,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
-  return res.json({
-    message: req,
-  })
+  return res.json(req)
 }
